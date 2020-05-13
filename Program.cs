945b4824c8d340a40b84623ad7eaa6edc965e827
Andre Bossard <abossard@gmail.com>
2020-05-13 11:27:55 +0200

    more comments

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace WaitAny
         /// <summary>
         /// No a good example of a long running task. The caller has no way to cancel or interrupt it and needs to resort to 
         /// </summary>
-        /// <returns></returns>
+        /// <returns>the answer</returns>
         public static Task<int> CreateInfiniteTask() {
             return Task.Run(async ()=>{
                 while (MostlyTrue)
@@ -24,6 +24,11 @@ namespace WaitAny
             });
         }
         
+        /// <summary>
+        /// A better example of how to implement a long running task
+        /// </summary>
+        /// <param name="ct"></param>
+        /// <returns></returns>
         public static Task<int> CreateInfiniteTaskWithCancellation(CancellationToken ct) {
             return Task.Run(async ()=>{
                 ct.ThrowIfCancellationRequested();
@@ -42,6 +47,14 @@ namespace WaitAny
             }, ct);
         }
         
+        /// <summary>
+        /// Wrap a Task that doesn't support cancellation so that the control flow goes back after a certain timeout.
+        /// Be aware: this doesn't cancel or stop the previous task. It just gives back the control flow and allows you to e.g. report on the issue (and maybe abort the process)
+        /// </summary>
+        /// <param name="taskToWrap">just any Task</param>
+        /// <param name="cancellationToken">to decide from outside, if a cancellation has happened</param>
+        /// <typeparam name="TResult">Result type</typeparam>
+        /// <returns></returns>
         public static async Task<TResult> WrapTaskWithCancellation<TResult>(Task<TResult> taskToWrap, CancellationToken cancellationToken)
         {
             // We create a TaskCompletionSource of decimal
@@ -74,10 +87,15 @@ namespace WaitAny
         
         private static async Task Main()
         {
+            // first we create the cancellation token source which we'll use in the next two examples
             var tokenSource = new CancellationTokenSource();
+            
+            // Example 1: When the job supports cancellation
+            // We set a timeout after which the jobs will be cancelled
             tokenSource.CancelAfter(TimeSpan.FromSeconds(1));
             try
             {
+                // we create the job and pass the token
                 var answer = await CreateInfiniteTaskWithCancellation(tokenSource.Token);
             }
             catch (TaskCanceledException taskCanceledException)
@@ -85,6 +103,7 @@ namespace WaitAny
                 Console.WriteLine("Task Cancelled after timeout");
             }
             
+            // Example 2: With the wrapping
             tokenSource = new CancellationTokenSource();
             tokenSource.CancelAfter(TimeSpan.FromSeconds(1));
             try
@@ -94,6 +113,8 @@ namespace WaitAny
             catch (TaskCanceledException taskCanceledException)
             {
                 Console.WriteLine("Task Cancelled after timeout");
+                // as the inner long running task can't be cancelled, it's highly suggested to exit the process
+                Environment.Exit(99);
             }
 
         }
